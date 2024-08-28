55b2aa7412e9d135efd39d33c1800f154169e283
Alexis12119 <corporal461@gmail.com>
2024-08-28 09:24:10 +0800

    feat(wezterm): change background opacity

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -67,7 +67,7 @@ return {
 	tab_bar_at_bottom = true,
 	use_fancy_tab_bar = true,
 	show_new_tab_button_in_tab_bar = false,
-	window_background_opacity = 1.0,
+	window_background_opacity = 0.8,
 	tab_max_width = 50,
 	hide_tab_bar_if_only_one_tab = true,
 	disable_default_key_bindings = false,
